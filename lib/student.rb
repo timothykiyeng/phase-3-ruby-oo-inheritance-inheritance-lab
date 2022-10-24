f0626a2f7019a166dbae2f04248cf692d4a264fd
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-10-24 15:19:39 +0300

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+ ## attr_accessor :knowledge
+      def initialize(knowledge=[])
+        @knowledge = knowledge
+      end
+  def learn(new_knowledge)
+    @knowledge << new_knowledge
+  end
+  def knowledge
+    return @knowledge
+  end
 
 end
